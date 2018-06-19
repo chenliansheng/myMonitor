9f546579ad5a8814d6d7ed89bac9573b753596a0
liansheng <a6@A6MACBOOKPRO>
2018-06-19 14:43:10 +0800

    monitorRedis

---
+++ myMonitorRedis.py
@@ -0,0 +1,48 @@
+#-*- coding: UTF-8 -*-
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+import os
+import redis
+
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr(( Header(name, 'utf-8').encode(), addr.encode('utf-8') if isinstance(addr, unicode) else addr))
+
+def monitorRedisAvailable():
+    redisHost = 'localhost'
+    redisPort = 63790
+    msg = 'Redis连接失败: host=' + redisHost+':'+str(redisPort)
+    try:
+        r = redis.Redis(host=redisHost, port=redisPort)
+        r.set("mymonitortestkey---------", "test")
+        r.get("mymonitortestkey---------")
+    except Exception as e:
+        msg = 'Redis连接失败: host='+redisHost
+        sendMail('liansheng <liansheng@comodin.cn>', msg, msg)
+
+    return
+
+
+def sendMail(p_to_addr,p_title,p_msg):
+    v_from_addr = 'mymaintain@comodin.cn'
+
+    smtp_server = 'smtp.exmail.qq.com'
+    password = 'Mypassword520'
+
+    msg = MIMEText(p_msg, 'plain', 'utf-8')
+    msg['From'] = _format_addr(u'maintain1 <%s>' % v_from_addr)
+    msg['To'] = _format_addr(u'liansheng <%s>' % p_to_addr)
+    #msg['CC'] = _format_addr(u'liansheng <%s>' % from_addr)
+    msg['Subject'] = Header(p_title, 'utf-8').encode()
+
+    server = smtplib.SMTP(smtp_server, 25)
+    # server.set_debuglevel(0)
+    server.login(v_from_addr, password)
+    server.sendmail(v_from_addr, [p_to_addr], msg.as_string())
+    server.quit()
+
+monitorRedisAvailable()
